4477042726d11f10b92fe68600db32da37ebd134
Jesse <jesseeluo0602@gmail.com>
2014-03-10 13:14:42 -0700

    add files for hw2

--- index.js
+++ index.js
@@ -1,13 +1,50 @@
 $(document).ready(function(){ 
  //INSERT CODE HERE
+console.log("JS loaded");
 
  //QUESTION 1
-
+    function Dog(){}
+    Dog.prototype.bark=function(){
+        return 'woof';
+    };
+    function ShibaInu(){
+    }
+    ShibaInu.prototype.constructor=Dog.prototype.constructor;
+    ShibaInu.prototype.constructor=ShibaInu;
+    ShibaInu.prototype.bark=function(){
+    return 'much wow very javascript';
+     };
  //QUESTION 3
+ 	$(".secondbutton").click(function(){
+ 		$(".secondheading").html("Penguins are the best!!!").css("color","turquoise");
+ 	});
 
  //QUESTION 4
+    $("#task4").click(function(){
+      $("#hipcat").css('-webkit-filter', 'blur(3px)');
+    });
 
  //QUESTION 5
 
+ $(".button").click(function(){
+    $(".wheel").css('-webkit-animation-play-state', 'running');
+    $("#wheel1").css('-webkit-animation-play-state', 'running');
+    $("#wheel2").css('-webkit-animation-play-state', 'running');
+ });
+
  //QUESTION 6
+ $(".nyancat").click(function() {
+    $(".nyancat").animate({
+    left: "+=1200"
+  }, 10000, function() {
+  });
+
+  $(".rainbows").animate({
+    left: "0",
+    width: "+=1200"
+  }, 10000, function() {
+  });
+
+ });
+
 });
